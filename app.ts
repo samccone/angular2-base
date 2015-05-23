79bcc7a3828c0f59ca2f920935d2b193d6050532
Sam Saccone <samccone@gmail.com>
2015-05-23 13:06:30 -0400

    Update to angular 25

--- app.ts
+++ app.ts
@@ -1,5 +1,5 @@
 /// <reference path="typings/angular2/angular2.d.ts" />
-import {Component, View, bootstrap, For, If} from 'angular2/angular2';
+import {Component, View, bootstrap, NgIf, NgFor} from 'angular2/angular2';
 
 class Store {
   todos: Array<{title: String, active: Boolean}>;
@@ -18,24 +18,24 @@ class Store {
 
 @Component({
   selector: 'sample-app',
-  injectables: [
+  hostInjector: [
     Store
   ]
 })
 @View({
-  directives: [For, If],
+  directives: [NgIf, NgFor],
   template: `
         <ul class="todo-list">
-          <li *for="#todo of store.todos" [class.active]="todo.active">
-            <p *if="todo.active" (click)="remove(todo)"> {{todo.title}}</p>
-            <p *if="!todo.active" (click)="remove(todo)"> wow not active</p>
+          <li ng-for="#todo of store.todos" [class.active]="todo.active">
+            <p ng-if="todo.active" (click)="remove(todo)"> {{todo.title}}</p>
+            <p ng-if="!todo.active" (click)="remove(todo)"> wow not active</p>
           </li>
         </ul>`
 })
 class SampleApp{
   store: Store;
-  constructor() {
-    this.store = new Store();
+  constructor(store: Store) {
+    this.store = store;
   }
   remove(todo: {title: String, active: Boolean}) {
     todo.active = !todo.active;
